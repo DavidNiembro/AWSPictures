178cbab254bf3a7bdc4969809181463357bbb417
David Niembro <david@niembro.ch>
2019-02-20 11:05:36 +0100

    Add image header in request

--- resources/views/galleries/show.blade.php
+++ resources/views/galleries/show.blade.php
@@ -5,8 +5,8 @@
     <ul>
     @foreach($gallery->pictures as $picture)
     <li>
-        <a href="{{ route('galleries.pictures.show', compact('gallery','picture'))}}">{{$picture->title}}
-            <img src="{{ route('galleries.pictures.show', compact('gallery','picture'))}}"/>
+        <a href="{{ route('galleries.pictures.show', compact('gallery','picture')) }}">{{$picture->title}}
+            <img src="{{ route('galleries.pictures.show', compact('gallery','picture')) }}.image"/>
         </a>
     </li>
     @endforeach
