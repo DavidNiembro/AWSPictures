67f93db26cf6ddf50c9837b572931d6a66a08a78
David Niembro <david@niembro.ch>
2019-03-20 11:28:50 +0100

    fix image show

--- resources/views/galleries/show.blade.php
+++ resources/views/galleries/show.blade.php
@@ -6,7 +6,7 @@
     @foreach($gallery->pictures as $picture)
     <li>
         <a href="{{ route('galleries.pictures.show', compact('gallery','picture')) }}">{{$picture->title}}
-            <img src="{{ route('galleries.pictures.show', compact('gallery','picture')) }}.image"/>
+            <img src="{{ route('galleries.pictures.show', compact('gallery','picture')) }}"/>
         </a>
     </li>
     @endforeach
