e40db287fd0b611c3b6aaef2d4b3bbab48cd8be2
David Niembro <david@niembro.ch>
2019-03-20 11:25:36 +0100

    Fix show pictures

--- resources/views/galleries/index.blade.php
+++ resources/views/galleries/index.blade.php
@@ -3,7 +3,7 @@
     <h1>Galleries</h1>
     <ul>
     @foreach($galleries as $gallery)
-        <li><a href={{route('galleries.show', $gallery->id)}}>{{$gallery->name}}</a</li>
+        <li><a href={{route('galleries.pictures.show', compact('gallery','picture'))}}>{{$gallery->name}}</a</li>
     @endforeach
     </ul
 @endsection
